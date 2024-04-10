546f26ff44efc98a87e5ea40bab80f6c0deef3a7
kartikeya pandey <kartikeyapandey20@gmail.com>
2024-04-10 09:46:18 +0530

    updated code for user id

--- api/v1/auth/repository.py
+++ api/v1/auth/repository.py
@@ -37,4 +37,4 @@ class AuthenticationRepository:
             )
             
         access_token = create_access_token(data={"user_id": user.id})
-        return {"access_token": access_token, "token_type": "bearer"}
+        return {"access_token": access_token, "token_type": "bearer" , "user_id": user.id}
